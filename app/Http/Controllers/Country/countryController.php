23cc0efa8a2a2bc6454b2ab6a53ae261497ad0a9
Aumlan Abrar <44971908+aumlan@users.noreply.github.com>
2021-02-06 11:13:21 +0600

    validation with Validator

--- app/Http/Controllers/Country/countryController.php
+++ app/Http/Controllers/Country/countryController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Country;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\countryModel;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+//use Validator;
 
 class countryController extends Controller
 {
@@ -18,7 +21,7 @@ class countryController extends Controller
     {
         $country = countryModel::find($id);
         if (is_null($country)) {
-            return response()->json('Record Not found', 404);
+            return response()->json(["message" => 'Record Not found'], 404);
         } else {
             return response()->json($country, 200);
         }
@@ -27,16 +30,33 @@ class countryController extends Controller
     //Restful api 201 -> Created
     public function countrySave(Request $request)
     {
-        $country = countryModel::create($request->all());
-        return response()->json($country, 201);
+        $rules = [
+            'name' => 'required|min:3',
+            'iso' => 'required|min:2|max:2'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        } else {
+            try {
+                $country = countryModel::create($request->all());
+                return response()->json($country, 201);
+            } catch (\Illuminate\Database\QueryException $e) {
+                $errorCode = $e->errorInfo[1];
+                if ($errorCode == 1062) {
+                    return response()->json(["message" => $e->errorInfo[2]], 400); //$e->errorInfo[2] = specific duplicate index and value
+                }
+            }
+        }
     }
+
 
     // Restful api 200 -> OK
     public function countryUpdate(Request $request, $id)
     {
         $country = countryModel::find($id);
         if (is_null($country)) {
-            return response()->json('Record Not found', 404);
+            return response()->json(["message" => 'Record Not found'], 404);
         } else {
             $country->update($request->all());
             return response()->json($country, 200);
@@ -48,7 +68,7 @@ class countryController extends Controller
     {
         $country = countryModel::find($id);
         if (is_null($country)) {
-            return response()->json('Record Not found', 404);
+            return response()->json(["message" => 'Record Not found'], 404);
         } else {
             $country->delete();
             return response()->json(null, 204);
